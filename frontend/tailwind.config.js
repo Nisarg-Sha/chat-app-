318f8dd4ebeda4e694cc75499c7e3f262a377226
Nisarg Shah <nisargshah@192.168.1.8>
2024-07-13 18:49:39 +0530

    deployment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,9 @@
-const { default: daisyui } = require('daisyui');
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-}
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	// eslint-disable-next-line no-undef
+	plugins: [require("daisyui")],
+};
